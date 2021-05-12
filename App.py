251b35d3ea21f4097862eb9bf5c68a37aa104709
mgrddsj <Jesse_Xu@live.com>
2021-05-12 16:31:45 +0800

    Display optimization for cropped small images

--- App.py
+++ App.py
@@ -14,14 +14,14 @@ file_path = st.sidebar.selectbox("Image:", file_list, index=8)
 image = cv2.imread(file_path)
 cropped = st_cropper(Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)))
 gray = cv2.cvtColor(np.array(cropped), cv2.COLOR_RGB2GRAY)
-st.image(gray, use_column_width=True, caption="灰度图")
+st.image(gray, caption="灰度图")
 
 # 最大滤波处理
 kernel = np.ones((3, 3), np.uint8)
 dilate_iteration = st.sidebar.slider(
     "最大滤波（膨涨）次数", min_value=1, max_value=50, value=25)
 dilate = cv2.dilate(gray, kernel, iterations=dilate_iteration)
-st.image(dilate, use_column_width=True, clamp=True, caption="最大滤波处理")
+st.image(dilate, caption="最大滤波处理")
 
 # 最小滤波处理
 kernel = np.ones((3, 3), np.uint8)
@@ -29,23 +29,22 @@ kernel = np.ones((3, 3), np.uint8)
 erosion_iteration = st.sidebar.slider(
     "最小滤波（腐蚀）次数", min_value=1, max_value=50, value=25)
 erosion = cv2.erode(dilate, kernel, iterations=erosion_iteration)
-st.image(erosion, use_column_width=True, clamp=True, caption="最小滤波处理")
+st.image(erosion, caption="最小滤波处理")
 
 # 阈值处理
 threshhold_value = st.sidebar.slider(
     "阈值", min_value=50, max_value=255, value=100)
 ret, thresh = cv2.threshold(erosion, threshhold_value, 255, cv2.THRESH_BINARY)
-thresh = thresh.astype(np.float64)
-st.image(thresh, use_column_width=True, clamp=True, caption="阈值处理")
+thresh = thresh.astype(np.uint8)
+st.image(thresh, caption="阈值处理")
 
 # 轮廓
-thresh = thresh.astype(np.uint8)
 # contours, hierarchy = cv2.findContours(
 #     thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # blank = np.zeros(thresh.shape, np.uint8)
 # img = cv2.drawContours(blank, contours, -1, (255, 255, 255), 1)
 img = cv2.Canny(thresh, 100, 200)
-st.image(img, use_column_width=True, clamp=True, caption="轮廓")
+st.image(img, caption="轮廓")
 # cv2.imwrite("contour.png", img)
 
 # 霍夫变换
@@ -62,8 +61,9 @@ lines = cv2.HoughLinesP(img, houghRho, np.pi/60, houghThreshhold,
 for line in lines:
     x1, y1, x2, y2 = line[0]
     cv2.line(blank2, (x1, y1), (x2, y2), (0, 255, 0), 2)
-st.image(blank2, use_column_width=True, clamp=True, caption="霍夫变换")
+st.image(blank2, caption="霍夫变换")
 st.text("lines detected: {}".format(len(lines)))
+st.text (lines)
 
 # Shi-Tomasi 角点检测
 corners_s = cv2.goodFeaturesToTrack(
@@ -73,6 +73,5 @@ shitomasi_img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 for i in corners_s:
     x, y = i.ravel()
     cv2.circle(shitomasi_img, (x, y), 10, (0, 255, 0), -1)
-st.image(shitomasi_img, use_column_width=True,
-         clamp=True, caption="Shi-Tomasi 角点检测")
+st.image(shitomasi_img, caption="Shi-Tomasi 角点检测")
 st.text("Corners detected: {}".format(len(corners_s)))
